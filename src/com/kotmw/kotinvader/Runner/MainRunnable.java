8649b4ae042591a549e80cb883438e2bee956d83
kotmw0701 <supurasyu@hotmail.co.jp>
2019-12-10 16:24:43 +0900

    タスク系の基礎を作成

---
+++ src/com/kotmw/kotinvader/Runner/MainRunnable.java
@@ -0,0 +1,12 @@
+package com.kotmw.kotinvader.Runner;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class MainRunnable implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
+}
