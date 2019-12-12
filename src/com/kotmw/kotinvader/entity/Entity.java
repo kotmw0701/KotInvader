88f7afd32fea54ee2449ebcf52bb649d10e44f72
kotmw0701 <supurasyu@hotmail.co.jp>
2019-12-12 11:26:29 +0900

    Entity周りの実装

--- src/com/kotmw/kotinvader/entity/Entity.java
+++ src/com/kotmw/kotinvader/entity/Entity.java
@@ -4,5 +4,49 @@ import javafx.scene.image.ImageView;
 
 public abstract class Entity extends ImageView {
 
+    private EntityType entityType;
+    private double speed;
+    private boolean alive;
+    private int hitPoints;
 
+    public Entity(String imagePath, double x, double y, EntityType entityType) {
+        this(imagePath, x, y, entityType, 0.0, entityType.getDefaultHitPoints());
+    }
+
+    public Entity(String imagePath, double x, double y, EntityType entityType, double speed, int hitPoints) {
+        super(imagePath);
+        this.setTranslateX(x);
+        this.setTranslateY(y);
+
+        this.entityType = entityType;
+        this.speed = speed;
+        this.hitPoints = hitPoints;
+    }
+
+    public boolean hit(int damage) {
+        hitPoints -= damage;
+        if (hitPoints < 0)
+            alive = false;
+        return alive;
+    }
+
+    public EntityType getEntityType() {
+        return entityType;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setEntityType(EntityType entityType) {
+        this.entityType = entityType;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
 }
---
+++ src/com/kotmw/kotinvader/entity/EntityType.java
@@ -0,0 +1,28 @@
+package com.kotmw.kotinvader.entity;
+
+public enum EntityType {
+    UNKNOWN(-1, 0),
+    CANNON(1, 1),
+    INVADER(1, 10),
+    UFO(1, 10),
+    CANNONMISSILE(1, 5),
+    INVADERMISSILE(1, 10),
+    Temp1(10, 10),
+    Temp2(50, 10),
+    Temp3(100, 10);
+
+    private int defaultHitPoints, damage;
+
+    EntityType(int defaultHitPoints, int damage) {
+        this.defaultHitPoints = defaultHitPoints;
+        this.damage = damage;
+    }
+
+    public int getDefaultHitPoints() {
+        return defaultHitPoints;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
