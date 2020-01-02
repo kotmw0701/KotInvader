a0e6aa376fa5086dde721f43eeee7a316a3fb8df
kotmw0701 <supurasyu@hotmail.co.jp>
2020-01-02 23:01:54 +0900

    開始した時に起動画面が消えなかったのを修正

--- src/com/kotmw/kotinvader/TitleController.java
+++ src/com/kotmw/kotinvader/TitleController.java
@@ -3,13 +3,16 @@ package com.kotmw.kotinvader;
 import com.kotmw.kotinvader.gui.GameMain;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class TitleController {
 
     @FXML
     private void onStart(ActionEvent actionEvent) {
         GameMain gameMain = new GameMain();
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        stage.close();
     }
 
     public void onSettings(ActionEvent actionEvent) {
