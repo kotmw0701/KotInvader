bb3b2bec0982181544cba03d12d2e4ac0c4d7335
kotmw0701 <supurasyu@hotmail.co.jp>
2020-01-03 00:16:56 +0900

    GameOver時の画面の実装(2)

--- src/com/kotmw/kotinvader/gui/CoverPane.java
+++ src/com/kotmw/kotinvader/gui/CoverPane.java
@@ -1,13 +1,47 @@
 package com.kotmw.kotinvader.gui;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CoverPane extends VBox {
 
-    public void CoverPane() {
+    private GameMain root;
+
+    public CoverPane(GameMain gameMain) {
+        this.root = gameMain;
         this.setPrefSize(GameMain.WINDOW_X, GameMain.WINDOW_Y);
         this.setFillWidth(false);
         this.setAlignment(Pos.CENTER);
+    }
+
+    public void showResult() {
+        BorderPane container = new BorderPane();
+        container.getStyleClass().add("result");
+        container.setPrefSize(500, GameMain.WINDOW_Y);
+        container.setPadding(new Insets(20));
+
+        Label top = new Label("Game Over");
+        BorderPane.setAlignment(top, Pos.CENTER);
+
+        VBox scoreData = new VBox();
+
+        HBox menu = new HBox();
+        menu.setAlignment(Pos.CENTER_RIGHT);
+        Button title = new Button("Title"), exit = new Button("Exit");
+        title.setOnAction(event -> root.close());
+        exit.setOnAction(event -> System.exit(0));
+
+        menu.getChildren().addAll(title, exit);
+
+        container.setTop(top);
+        container.setCenter(scoreData);
+        container.setBottom(menu);
+
+        getChildren().add(container);
     }
 }
