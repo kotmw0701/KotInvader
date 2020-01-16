3c85b5dbcc148b8e986d2e125300c0d30d8eabfd
kotmw0701 <supurasyu@hotmail.co.jp>
2020-01-16 11:44:31 +0900

    Test画面でテスト中

--- src/com/kotmw/kotinvader/TestStage.java
+++ src/com/kotmw/kotinvader/TestStage.java
@@ -1,14 +1,12 @@
 package com.kotmw.kotinvader;
 
-import javafx.animation.Interpolator;
-import javafx.animation.RotateTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.SequentialTransition;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -16,6 +14,9 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestStage extends Stage {
 
@@ -33,38 +34,42 @@ public class TestStage extends Stage {
         root.getStyleClass().add("testClass");
 
         //アニメーションするNode
-        Pane pane = new Pane();
-        pane.setPrefWidth(1200);
-        pane.setPrefHeight(700);
-        pane.getStyleClass().add("testClass2");
+        TilePane tilePane = new TilePane();
+        tilePane.setPrefWidth(1200);
+        tilePane.setPrefHeight(700);
+        tilePane.getStyleClass().add("testClass");
 
-        Circle circle = new Circle(100, 100, 50, Color.BLACK);
+        List<Transition> transitions = new ArrayList<>();
 
-        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(10), circle);
-        rotateTransition.setAxis(new Point3D(0, 1, 0));
-        rotateTransition.setFromAngle(0.0);
-        rotateTransition.setToAngle(360.0*10);
-        rotateTransition.setCycleCount(1);
-        rotateTransition.setInterpolator(Interpolator.LINEAR);
+        for (int i = 0; i < 12*7; i++) {
+            Pane pane = new Pane();
+            pane.setPrefSize(100, 100);
+            pane.setStyle("-fx-background-color: gray");
+            pane.setOpacity(0.0);
 
-        //拡大のアニメーション、X広がりとY広がりを順番にやりたいため2つに分割
-        ScaleTransition animationX = new ScaleTransition(Duration.seconds(0.25), pane), animationY = new ScaleTransition(Duration.seconds(0.25), pane);
-        animationX.setFromX(0.001);
-        animationX.setToX(1.0);
-        animationY.setFromY(0.005);
-        animationY.setToY(1.0);
-        animationY.setInterpolator(Interpolator.EASE_OUT);
+            RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.1), pane);
+            rotateTransition.setAxis(new Point3D(1, 1, 0));
+            rotateTransition.setFromAngle(90.0);
+            rotateTransition.setToAngle(0.0);
+            rotateTransition.setInterpolator(Interpolator.SPLINE(0.55, 0.055, 0.675, 0.19));
 
-        SequentialTransition animation = new SequentialTransition(animationX, animationY, rotateTransition);
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.1), pane);
+            fadeTransition.setFromValue(0.3);
+            fadeTransition.setToValue(1.0);
 
-        pane.getChildren().add(circle);
-        root.getChildren().addAll(pane);
+            transitions.add(new ParallelTransition(rotateTransition, fadeTransition));
+            tilePane.getChildren().add(pane);
+        }
+
+        SequentialTransition sequentialTransition = new SequentialTransition(transitions.toArray(new Transition[0]));
+
+        root.getChildren().addAll(tilePane);
         Scene scene = new Scene(root);
         Application.setUserAgentStylesheet("MODENA");
         scene.getStylesheets().addAll(getClass().getResource("/resources/main.css").toExternalForm());
         scene.setFill(null);
 
-        animation.play();
+        sequentialTransition.play();
 
         this.setScene(scene);
         this.initStyle(StageStyle.TRANSPARENT);
