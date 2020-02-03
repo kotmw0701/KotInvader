d842a51c60226160a5ab9988fb8b6206589c4786
kotmw0701 <supurasyu@hotmail.co.jp>
2020-02-03 14:25:27 +0900

    タイトル画面のアニメーションの変更

--- src/com/kotmw/kotinvader/TitleController.java
+++ src/com/kotmw/kotinvader/TitleController.java
@@ -7,8 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,77 +19,76 @@ public class TitleController implements Initializable {
     @FXML
     private Label title;
     @FXML
-    private Pane start, settings, exit;
-//    public VBox animContainer;
-
-    /*
-
-    Title:
-        Pause → Fade → Translate → Translate → Fade
-
-    Button:
-        Translate → |
-        Fade      → |
-                     Translate → |
-                     Fade      → |
-                                  Translate → |
-                                  Fade      → |
-
-    SequentialTransition
-     - PauseTransition
-     - FadeTransition
-     - TranslateTransition
-     - ParallelTransition
-        - ParallelTransition
-          - TranslateTransition
-          - FadeTransition
-          - TranslateTransition
-          - FadeTransition
-        - ParallelTransition
-          - TranslateTransition
-          - FadeTransition
-          - TranslateTransition
-          - FadeTransition
-        - ParallelTransition
-          - TranslateTransition
-          - FadeTransition
-          - TranslateTransition
-          - FadeTransition
-     */
-
+    private Button start, settings, exit;
+    @FXML
+    private Rectangle startBack, settingsBack, exitBack;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Pane[] panes = new Pane[]{start, settings, exit};
+        Button[] buttons = new Button[]{start, settings, exit};//Buttons
+        Rectangle[] rectangles = new Rectangle[]{startBack, settingsBack, exitBack};
         ParallelTransition buttonParallel = new ParallelTransition();
         for (int p = 0; p < 3; p++) {
-            Pane pane = panes[p];
-            pane.setOpacity(0.0);
-            FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.1), pane);
+            Button button = buttons[p];
+            button.setOpacity(0.0);
+            rectangles[p].setScaleX(0.0);
+            TranslateTransition translate = new TranslateTransition(Duration.seconds(0.3), button);
+            translate.setFromX(-40.0);
+            translate.setToX(0.0);
+            translate.setInterpolator(Interpolator.EASE_OUT);
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5), button);
             fadeIn.setFromValue(0.0);
             fadeIn.setToValue(1.0);
-            ParallelTransition parallelTransition = new ParallelTransition();
-            for (int i = 0; i < 2; i++) {
-                Region region = new Region();
-                TranslateTransition slide = new TranslateTransition(Duration.seconds(0.5), region);
-                slide.setInterpolator(Interpolator.SPLINE(0.55, 0.055, 0.675, 0.19)); //easeInCubic
-                FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), region);
-                fadeTransition.setFromValue(1.0);
-                fadeTransition.setToValue(0.0);
-                fadeTransition.setOnFinished(event -> region.setVisible(false));
-                if (i == 0) {
-                    region.getStyleClass().add("left");
-                    slide.setFromX(0.0);
-                    slide.setToX(-100.0);
-                } else {
-                    region.getStyleClass().add("right");
-                    slide.setFromX(100.0);
-                    slide.setToX(200.0);
+            buttonParallel.getChildren().addAll(
+                    new SequentialTransition(new PauseTransition(Duration.seconds(0.2*p)), new ParallelTransition(fadeIn, translate)));
+            button.setOnMouseEntered(event -> {
+                Rectangle rectangle;
+                switch (((Button)event.getSource()).getId()) {
+                    case "start":
+                        rectangle = startBack;
+                        break;
+                    case "settings":
+                        rectangle = settingsBack;
+                        break;
+                    case "exit":
+                        rectangle = exitBack;
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + ((Button) event.getSource()).getId());
                 }
-                pane.getChildren().add(region);
-                parallelTransition.getChildren().addAll(slide, fadeTransition);
-            }
-            buttonParallel.getChildren().add(new SequentialTransition(fadeIn, parallelTransition));
+                TranslateTransition backTranslate = new TranslateTransition(Duration.seconds(0.3), rectangle);
+                backTranslate.setFromX(-150);
+                backTranslate.setToX(0.0);
+                ScaleTransition scale = new ScaleTransition(Duration.seconds(0.3), rectangle);
+                scale.setFromX(0.0);
+                scale.setToX(1.0);
+                ParallelTransition parallel = new ParallelTransition(backTranslate, scale);
+                parallel.play();
+            });
+            button.setOnMouseExited(event -> {
+                Rectangle rectangle;
+                switch (((Button)event.getSource()).getId()) {
+                    case "start":
+                        rectangle = startBack;
+                        break;
+                    case "settings":
+                        rectangle = settingsBack;
+                        break;
+                    case "exit":
+                        rectangle = exitBack;
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + ((Button) event.getSource()).getId());
+                }
+                TranslateTransition backTranslate = new TranslateTransition(Duration.seconds(0.3), rectangle);
+                backTranslate.setFromX(0.0);
+                backTranslate.setToX(-150);
+                ScaleTransition scale = new ScaleTransition(Duration.seconds(0.3), rectangle);
+                scale.setFromX(1.0);
+                scale.setToX(0.0);
+                ParallelTransition parallel = new ParallelTransition(backTranslate, scale);
+                parallel.play();
+            });
         }
 
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.8), title);
